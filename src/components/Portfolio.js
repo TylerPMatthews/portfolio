9063349efff348874e2490286d9c2520662e0213
ty <tylerpmatthews1998@gmail.com>
2021-04-26 21:29:56 -0400

    Added medical cannabis project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,6 +13,7 @@ import projectImage1 from "../images/meme.png";
 import projectImage2 from "../images/construction.jpg";
 import projectImage3 from "../images/yeezy.png";
 import projectImage4 from '../images/imonline.png'
+import projectImage5 from '../images/green.png'
 
 const useStyles = makeStyles((theme) => ({
   mainContainer: {
@@ -27,36 +28,37 @@ const useStyles = makeStyles((theme) => ({
 
 const projects = [
   {
+    name: "Green Orchard",
+    description: `Full-Stack medical cannabis web application. EASY SIGN IN: username: test  password: test. Back-end may take a few seconds on initial load due to it being hosted on free Heroku`,
+    image: "https://images.unsplash.com/photo-1491422179097-9b27aa13128a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1951&q=80",
+    github: "https://github.com/users/TylerPMatthews/projects/5",
+    live: "https://medical-front-end.vercel.app",
+  },
+  {
     name: "IMONLINE",
     description: `Full stack social media application.--- Easy sign in : USERNAME: test  PASSWORD: test ---  Site may take a few seconds on initial load due to my back-end being hosted on free Heroku`,
-    image: projectImage4,
+    image: "https://images.unsplash.com/photo-1522204523234-8729aa6e3d5f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
     github: "https://github.com/users/TylerPMatthews/projects/4",
     live: "https://im-online-frontend.vercel.app",
   },
   {
     name: "Redux Yeezy Shoes (CURRENTLY MOBILE ONLY)",
     description: `Yeezy Shoes is a full-stack project. The project is a store which a user can purchase Yeezy shoes. React and Redux are being used to run the front-end. Node and Postgres are maintaining the back-end. Site may take a few seconds on initial load due to my back-end being hosted on free Heroku`,
-    image: projectImage3,
+    image: "https://images.unsplash.com/photo-1515110371136-7e393289662c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1956&q=80",
     github: "https://github.com/users/TylerPMatthews/projects/3",
     live: "https://yeezyshoesfrontend.vercel.app",
   },
   {
     name: "Meme Weather",
     description: `Meme Weather is my first full-stack project. This project
-    uses two APIs, one for getting weather imformation and another (created by me) displaying the meme. I am using
+    uses two APIs, one for getting weather information and another (created by me) displaying the meme. I am using
     React for my front-end and Node for my backend. Site may take a few seconds on initial load due to my back-end being hosted on free Heroku`,
     image: projectImage1,
     github: "https://github.com/users/TylerPMatthews/projects/2",
     live: "https://meme-weather-frontend.vercel.app",
   },
 
-  {
-    name: "Coming soon ...",
-    description: `More projects coming soon ...`,
-    image: projectImage2,
-    github: "https://github.com/TylerPMatthews/",
-    live: "",
-  },
+ 
   // {
   //   name: "Project 5",
   //   description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis\
